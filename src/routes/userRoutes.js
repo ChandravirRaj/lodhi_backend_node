ec11fcf6a8577d6e5f77f1cb202e5cd21deb90c1
chandravir <chandravir.singh@mobcoder.com>
2025-05-18 00:37:19 +0530

    add auth middleware and logout api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { registerUser, loginUser, fetchProfile, getAllUsers } = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
-router.post('/register', registerUser); // Register a new user
-router.post('/login',loginUser);   // User login
-router.post('/profile',authMiddleware,fetchProfile);  // get user profile from database
-router.get('/getAllUsers',getAllUsers); // getAllUsers from Database
+router.post('/register', userController.registerUser); // Register a new user
+router.post('/login',userController.loginUser);   // User login
+router.post('/profile',authMiddleware,userController.fetchProfile);  // get user profile from database
+router.get('/getAllUsers',userController.getAllUsers); 
+router.post('/logoutUser',authMiddleware,userController.logoutUser);// getAllUsers from Database
 
 module.exports = router;
 
